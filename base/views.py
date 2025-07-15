524a0c6f88fd39a7eb961d6732fbb02b29e846b2
Preeti Mallick <preetimallick2001@gmail.com>
2025-07-15 20:42:46 +0530

    register_page

--- base/views.py
+++ base/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+#yellow_underline - currently vscode , this project is using system python software, from system python software we are using for download venv module
+# this underline means changing python interpreter - this point should kept in mind
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
