41a515b4b1a3825f74945c87978bc80d22e62d4d
Preeti Mallick <preetimallick2001@gmail.com>
2025-07-17 22:05:20 +0530

    reset_password

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('',views.login_,name='login'),
     path('register/',views.register_,name='register'),
     path('logout/',views.logout_,name='logout'),
+    path('profile/',views.profile,name='profile'),
+    path('update_profile',views.update_profile,name='update_profile'),
+    path('change_pass',views.change_pass,name='change_pass'),
 ]
